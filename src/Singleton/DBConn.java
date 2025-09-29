5d76cc1d68537bc66b9fd8fb6b731669fbf24c95
Leonardo Meneses Cavalcante <leonardo_meneses@virgo.atlantico.net.br>
2025-09-29 18:04:21 -0300

    singleton

--- src/Singleton/DBConn.java
+++ src/Singleton/DBConn.java
@@ -1,4 +1,18 @@
 package Singleton;
 
 public class DBConn {
+
+    private DBConn() {}
+
+    private static DBConn Instance;
+
+    public static DBConn getInstance() {
+        if (Instance == null) {
+            Instance = new DBConn();
+        }
+        return Instance;
+    }
+    public void connect(String message) {
+        System.out.println(message);
+    }
 }
--- src/Singleton/MainSingleton.java
+++ src/Singleton/MainSingleton.java
@@ -3,6 +3,9 @@ package Singleton;
 public class MainSingleton {
     public static void main(String[] args) {
         Logger lg = Logger.getInstance();
-        lg.log("MainSingleton");
+        lg.log("Log message");
+
+        DBConn db = DBConn.getInstance();
+        db.connect("Db connected");
     }
 }
